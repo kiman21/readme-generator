b016ad47e63ee584e454486e42366cef9160fd6d
kiman21 <kim.andrew0121@gmail.com>
2023-04-10 21:10:41 -0700

    yeah final

--- assets/index.js
+++ assets/index.js
@@ -3,10 +3,9 @@ const fs = require("fs");
 
 inquirer.prompt([
         {
-            type: "list",
-            name: "licensing",
-            message: "Select a license for your project?",
-            choices:["MIT", "Apache 2.0", "GPL 3.0"]
+            type:"input",
+            name:"license",
+            message:"Provide licensing details. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).",
         },
         {
             type: "list",
@@ -54,16 +53,11 @@ inquirer.prompt([
             name:"credits",
             message:"List your collaborators, if any, with links to their GitHub profiles. If you used any third-party assets that require attribution, list the creators with links to their primary web presence.",
         },
-        {
-            type:"input",
-            name:"license",
-            message:"Provide licensing details. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).",
-        },
 ])
 .then((response)=>{
     fs.writeFile("NEWREADME.md", 
-    `# License: ${answers.licensing}
-    ![${answers.licensing}](https://imgshields.io/badge/${answers.badge})
+    `# License: ${answers.license}
+    ![${answers.license}](https://imgshields.io/badge/${answers.badge})
     # ${response.projectName}
 
     
@@ -88,11 +82,8 @@ inquirer.prompt([
 
     ## Credits
     
-    ${response.credits}
+    ${response.credits}`,
     
-    ## License
-    
-    ${response.license}`,
     (err) => {
         if (err) {
               console.log(err);
